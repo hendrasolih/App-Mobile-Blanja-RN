7b2f3ffba7204e527d5f0373c9a5487bda74e971
hendrasolih <hendra.solih.jp@gmail.com>
2021-02-11 19:59:10 +0700

    token expired

--- src/screens/Splash/index.js
+++ src/screens/Splash/index.js
@@ -2,12 +2,42 @@ import React, {useEffect} from 'react';
 import {StyleSheet, View, Image} from 'react-native';
 import {SplashLogo} from '../../assets';
 
-const Splash = ({navigation}) => {
+//redux
+import {connect, useSelector} from 'react-redux';
+import {logout} from '../../utils/redux/action/authAction';
+import {API_URL} from '@env';
+import axios from 'axios';
+
+const Splash = ({navigation, logoutRedux}) => {
+  const token = useSelector((state) => state.auth.token);
   useEffect(() => {
-    setTimeout(() => {
-      navigation.replace('MainApp');
-    }, 3000);
+    checktoken();
+    // setTimeout(() => {
+    //   navigation.replace('MainApp');
+    // }, 3000);
   }, [navigation]);
+  const checktoken = () => {
+    const config = {
+      headers: {
+        'x-access-token': 'Bearer ' + token,
+      },
+    };
+    axios
+      .get(`${API_URL}/auth/checktoken`, config)
+      .then((res) => {
+        console.log(res.data);
+        navigation.replace('MainApp');
+      })
+      .catch((error) => {
+        if (error.response) {
+          console.log(error.response.data);
+          console.log(error.response.status);
+          //console.log(error.response.headers);
+        }
+        logoutRedux();
+        navigation.replace('MainApp');
+      });
+  };
   return (
     <View style={styles.background}>
       <Image source={SplashLogo} />
@@ -15,7 +45,13 @@ const Splash = ({navigation}) => {
   );
 };
 
-export default Splash;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    logoutRedux: () => dispatch(logout()),
+  };
+};
+
+export default connect(null, mapDispatchToProps)(Splash);
 
 const styles = StyleSheet.create({
   background: {
